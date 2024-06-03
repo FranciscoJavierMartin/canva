7599d41ef069ec4e66c491c27b10bcb984632def
Francisco <javiermartincuenca@gmail.com>
2024-06-03 19:51:15 +0200

    Initial commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,5 @@
-import { component$, Slot } from "@builder.io/qwik";
-import type { RequestHandler } from "@builder.io/qwik-city";
+import { component$, Slot } from '@builder.io/qwik';
+import type { DocumentHead, RequestHandler } from '@builder.io/qwik-city';
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -10,6 +10,16 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
     // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
     maxAge: 5,
   });
+};
+
+export const head: DocumentHead = {
+  title: 'Canva',
+  meta: [
+    {
+      name: 'description',
+      content: 'App for make your own drawings',
+    },
+  ],
 };
 
 export default component$(() => {
