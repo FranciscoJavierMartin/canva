a256a8c9f5e6541b8ca60c82735caf9103bb96a4
Francisco <javiermartincuenca@gmail.com>
2024-06-03 21:27:59 +0200

    Add logo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,16 @@
 import { component$ } from '@builder.io/qwik';
+import Logo from '@/components/icons/logo';
 
 export default component$(() => {
   return (
-    <>
-      <h1 class='text-4xl'>Canva</h1>
-    </>
+    <div class='min-h-screen w-full bg-black'>
+      <div class='bg-mid-black shadow-md'>
+        <div class='m-auto w-[93%] py-3'>
+          <div class='h-[30px] w-[80px]'>
+            <Logo />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 });
