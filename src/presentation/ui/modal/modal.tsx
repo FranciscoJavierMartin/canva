7895308ac02ae3a887cb59c5ac1cfee140877c6a
Francisco <javiermartincuenca@gmail.com>
2024-06-04 20:04:23 +0200

    Fix class

--- src/presentation/ui/modal/modal.tsx
+++ src/presentation/ui/modal/modal.tsx
@@ -1,5 +1,6 @@
 import { $, component$, useContext, useOnWindow } from '@builder.io/qwik';
 import { PortalCloseAPIContextId } from '@/presentation/contexts/portal-close';
+import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
   const portalClose = useContext(PortalCloseAPIContextId);
@@ -15,8 +16,11 @@ export default component$(() => {
 
   return (
     <div class='fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-[#252627ad] transition-all duration-500'>
-      <div class='popup-example1 relative m-auto w-[350px] rounded-md bg-[#323335] px-6 py-4'>
-        <div class='absolute right-4 top-4 cursor-pointer text-xl text-white'></div>
+      <div class='relative m-auto w-[350px] rounded-md bg-[#323335] px-6 py-4'>
+        <Link
+          href='..'
+          class='absolute right-4 top-4 cursor-pointer text-xl text-white'
+        ></Link>
         <h1 class='text-white'>Modal</h1>
       </div>
     </div>
