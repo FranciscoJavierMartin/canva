dc01a3f4d2085db9d91a65b1dc1da723a9c7fb35
Francisco <javiermartincuenca@gmail.com>
2024-06-05 21:17:59 +0200

    Move to global action

--- src/presentation/components/forms/login-form.tsx
+++ src/presentation/components/forms/login-form.tsx
@@ -1,7 +1,33 @@
-import InputForm from '@/presentation/ui/inputs/input-form';
-import { useLoginUser } from '@/routes';
 import { component$ } from '@builder.io/qwik';
-import { Form } from '@builder.io/qwik-city';
+import {
+  Form,
+  type RequestEventAction,
+  globalAction$,
+  z,
+  zod$,
+} from '@builder.io/qwik-city';
+import InputForm from '@/presentation/ui/inputs/input-form';
+
+export const useLoginUser = globalAction$(
+  async (data, requestEvent: RequestEventAction) => {
+    console.log(data);
+
+    return {
+      message: '',
+      // errors: {},
+    };
+  },
+  zod$({
+    email: z
+      .string({ required_error: 'Email is required' })
+      .trim()
+      .email('Invalid email'),
+    password: z
+      .string({ required_error: 'Password is required' })
+      .trim()
+      .min(6, 'Password is too short'),
+  }),
+);
 
 export default component$(() => {
   const loginUser = useLoginUser();
@@ -9,15 +35,28 @@ export default component$(() => {
   return (
     <>
       <h2 class='text-center text-2xl text-white'>Sign In</h2>
-      <Form action={loginUser} class='mb-3 flex flex-col gap-2'>
-        <InputForm name='email' id='email' placeholder='Email' type='email' />
+      <Form action={loginUser} class='mb-3 flex flex-col gap-2' >
         <InputForm
-          name='password'
-          id='password'
-          placeholder='Password'
-          type='password'
+          id='email'
+          name='email'
+          type='email'
+          placeholder='Email'
+          errors={loginUser.value?.fieldErrors?.email}
         />
-        <button class='mt-6 w-full rounded-md bg-purple-500 px-3 py-2 font-semibold text-white outline-none hover:bg-purple-600'>
+        <InputForm
+          id='password'
+          name='password'
+          type='password'
+          placeholder='Password'
+          errors={loginUser.value?.fieldErrors?.password}
+        />
+        <p class='my-5'>
+          {JSON.stringify(loginUser.value?.fieldErrors?.password)}
+        </p>
+        <button
+          disabled={loginUser.isRunning}
+          class='mt-6 w-full rounded-md bg-purple-500 px-3 py-2 font-semibold text-white outline-none hover:bg-purple-600 disabled:bg-purple-400'
+        >
           Sign In
         </button>
       </Form>
