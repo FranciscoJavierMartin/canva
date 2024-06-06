03d7c4d220d5af3641a506fef9bcb8e5a27c534a
Francisco <javiermartincuenca@gmail.com>
2024-06-06 18:48:59 +0200

    Navbar for home

--- src/presentation/ui/Navbar.tsx
+++ src/presentation/ui/Navbar.tsx
@@ -1,4 +1,4 @@
-import { component$ } from '@builder.io/qwik';
+import { Slot, component$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
 import Logo from '../icons/logo';
 
@@ -11,16 +11,7 @@ export default component$(() => {
             <Logo />
           </Link>
           <div class='flex items-center justify-center gap-4'>
-            <button class='button bg-light-purple px-2 hover:bg-purple-bright'>
-              Create a design
-            </button>
-            <div>
-              <img
-                class='h-[45px] w-[48px] rounded-full'
-                src='https://templates-flatlogic.herokuapp.com/sing-app/html5/demo/img/people/a5.jpg'
-                alt='User avatar'
-              />
-            </div>
+            <Slot />
           </div>
         </div>
       </div>
--- src/routes/home/layout.tsx
+++ src/routes/home/layout.tsx
@@ -4,7 +4,18 @@ import Navbar from '@/presentation/ui/Navbar';
 export default component$(() => {
   return (
     <div class='min-h-screen w-full bg-black'>
-      <Navbar />
+      <Navbar>
+        <button class='button bg-light-purple px-2 hover:bg-purple-bright'>
+          Create a design
+        </button>
+        <img
+          class='size-[45px] rounded-full'
+          src='https://templates-flatlogic.herokuapp.com/sing-app/html5/demo/img/people/a5.jpg'
+          alt='User avatar'
+          height={45}
+          width={45}
+        />
+      </Navbar>
       <Slot />
     </div>
   );
