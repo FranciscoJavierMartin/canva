422aafb07fc9af834017707c0508bacdf4e8a783
Francisco <javiermartincuenca@gmail.com>
2024-06-06 18:45:55 +0200

    Fix width in navbar

--- src/presentation/ui/Navbar.tsx
+++ src/presentation/ui/Navbar.tsx
@@ -5,7 +5,7 @@ import Logo from '../icons/logo';
 export default component$(() => {
   return (
     <div class='w-full bg-mid-black shadow-md'>
-      <div class='m-auto w-[93%] py-3'>
+      <div class='m-auto p-3'>
         <div class='flex items-center justify-between'>
           <Link href='/home' class='h-[30px] w-[80px]'>
             <Logo />
