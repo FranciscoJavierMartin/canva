4b2adb8260dffca2dc49ed75192283f3eafffbb5
Francisco <javiermartincuenca@gmail.com>
2024-06-06 19:31:23 +0200

    Change tags

--- src/presentation/ui/Navbar.tsx
+++ src/presentation/ui/Navbar.tsx
@@ -4,17 +4,17 @@ import Logo from '../icons/logo';
 
 export default component$(() => {
   return (
-    <div class='w-full bg-mid-black shadow-md'>
+    <header class='w-full bg-mid-black shadow-md'>
       <div class='m-auto p-3'>
-        <div class='flex items-center justify-between'>
+        <nav class='flex items-center justify-between'>
           <Link href='/home' class='h-[30px] w-[80px]'>
             <Logo />
           </Link>
           <div class='flex items-center justify-center gap-4'>
             <Slot />
           </div>
-        </div>
+        </nav>
       </div>
-    </div>
+    </header>
   );
 });
