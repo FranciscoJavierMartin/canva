be0a4672a7b77f8571b898a5e0e9ffa2f5acd8ac
Francisco <javiermartincuenca@gmail.com>
2024-06-07 21:44:31 +0200

    Align text in number

--- src/presentation/ui/inputs/input-number.tsx
+++ src/presentation/ui/inputs/input-number.tsx
@@ -7,10 +7,13 @@ type InputNumber = QwikIntrinsicElements['input'] & {
 export default component$<InputNumber>(({ text, ...props }) => {
   return (
     <div class='flex flex-col items-start justify-center gap-2'>
-      <label for={props.id}>{text}</label>
+      <label for={props.id} class='-mb-1 ml-2'>
+        {text}
+      </label>
       <input
         type='number'
-        class='w-full rounded-md border border-[#404040] bg-[#1b1a1a] px-2 py-1 outline-none'
+        class='w-full rounded-md border border-[#404040] bg-[#1b1a1a] px-2 py-1 text-right outline-none placeholder:text-left'
+        max={100_000}
         {...props}
       />
     </div>
