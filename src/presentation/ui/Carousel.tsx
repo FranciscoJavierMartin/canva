5cde653d2df1b5dc5633fd06a12fa589c2e4b91d
Francisco <javiermartincuenca@gmail.com>
2024-06-08 11:39:39 +0200

    Style carousel

--- src/presentation/ui/Carousel.tsx
+++ src/presentation/ui/Carousel.tsx
@@ -1,9 +1,25 @@
 import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+import Trash from '@/presentation/icons/trash';
 
 export default component$(() => {
   return (
-    <div>
-      <h1>Carousel</h1>
+    <div class='no-scrollbar inline-flex snap-mandatory overflow-x-scroll scroll-smooth'>
+      <div class='box-content flex flex-none snap-start'>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((d) => (
+          <div key={d} class='group relative h-[170px] w-full px-2'>
+            <Link href='/' class='block size-full rounded-md bg-slate-100 p-2'>
+              <img
+                src='https://media.cntraveler.com/photos/56e20de2a69cef316dc99c9f/master/pass/neuschwanstein-castle-germany-cr-getty.jpg'
+                class='size-full overflow-hidden rounded-md'
+              />
+            </Link>
+            <div class='absolute right-5 top-3 hidden cursor-pointer rounded-md bg-white p-1 text-red-500 transition-all duration-500 group-hover:block'>
+              <Trash />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 });
