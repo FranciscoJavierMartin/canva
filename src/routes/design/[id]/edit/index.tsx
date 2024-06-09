85fc3ccee88a48861d3a8efe52e6a0af9d72194e
Francisco <javiermartincuenca@gmail.com>
2024-06-09 23:02:47 +0200

    Works

--- src/routes/design/[id]/edit/index.tsx
+++ src/routes/design/[id]/edit/index.tsx
@@ -5,29 +5,28 @@ import ArrowLeft from '@/presentation/icons/arrow-left';
 
 export default component$(() => {
   return (
-    <div class='min-w-screen h-screen bg-black'>
+    <div class='min-w-screen edit-page-container h-screen bg-black'>
       <Navbar>
         <div class='flex items-center justify-center gap-2 text-gray-200'>
           <button class='button bg-purple-blue px-3'>Save</button>
           <button class='button bg-purple-bright px-3'>Download</button>
         </div>
       </Navbar>
-      <div class='flex h-[calc(100vh-64px)]'>
-        <Sidebar />
-        <div
-          class={['bg-black-light relative z-10 w-[350px]', { 'p-0': true }]}
-        >
-          <button class='bg-black-light absolute -right-2 top-[calc(50%-50px)] flex h-[100px] w-[15px] items-center justify-center rounded-full text-slate-300 hover:text-gray-100'>
-            <ArrowLeft />
-          </button>
-        </div>
-        <div class='h-full w-10'></div>
-        <div class='bg-black-light h-full w-[250px] px-3 py-2 text-gray-300'>
-          <div class='flex h-full flex-col items-start justify-start gap-6 px-3'>
-
-          </div>
+      {/* <div class='flex h-[calc(100vh-64px)]'> */}
+      <Sidebar />
+      <div class={['bg-black-light relative z-10 w-[350px]', { 'p-0': true }]}>
+        <button class='bg-black-light absolute -right-2 top-[calc(50%-50px)] flex h-[100px] w-[15px] items-center justify-center rounded-full text-slate-300 hover:text-gray-100'>
+          <ArrowLeft />
+        </button>
+      </div>
+      <div class='h-full w-10'></div>
+      {/* <div class='bg-black-light h-full w-[250px] px-3 py-2 text-gray-300'> */}
+      <div class='h-full w-[250px] bg-blue-400 px-3 py-2 text-gray-300'>
+        <div class='flex h-full flex-col items-start justify-start gap-6 px-3'>
+          <h1 class='text-red-400'>Hello world</h1>
         </div>
       </div>
+      {/* </div> */}
     </div>
   );
 });
