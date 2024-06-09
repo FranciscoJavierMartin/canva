f10de45d052e4bad91b1eb3b53304fa560c7623f
Francisco <javiermartincuenca@gmail.com>
2024-06-09 18:38:44 +0200

    WIP

---
+++ src/presentation/icons/arrow-left.tsx
@@ -0,0 +1,19 @@
+import { component$ } from '@builder.io/qwik';
+import type { IconProps } from '@/interfaces/types/icons';
+
+export default component$<IconProps>(({ styles }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke-width='1.5'
+    stroke='currentColor'
+    class={['size-6', styles]}
+  >
+    <path
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      d='M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18'
+    />
+  </svg>
+));
--- src/routes/design/[id]/edit/index.tsx
+++ src/routes/design/[id]/edit/index.tsx
@@ -1,6 +1,7 @@
 import { component$ } from '@builder.io/qwik';
 import Navbar from '@/presentation/ui/Navbar';
 import Sidebar from '@/presentation/components/sidebar/sidebar';
+import ArrowLeft from '@/presentation/icons/arrow-left';
 
 export default component$(() => {
   return (
@@ -11,9 +12,13 @@ export default component$(() => {
           <button class='button bg-purple-bright px-3'>Download</button>
         </div>
       </Navbar>
-      <Sidebar />
-      <div class='h-full'>
-        
+      <div class='h-full flex'>
+        <Sidebar />
+        <div class='bg-black-light z-10 w-[350px]'>
+          <button class='bg-black-light flex h-[100px] w-[20px] items-center justify-center rounded-full text-slate-300'>
+            <ArrowLeft />
+          </button>
+        </div>
       </div>
     </div>
   );
