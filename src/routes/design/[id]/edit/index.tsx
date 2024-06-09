1cd6064da9d986d41059f495f41cf889582878ed
Francisco <javiermartincuenca@gmail.com>
2024-06-09 22:44:06 +0200

    Place arrow in sidebar

--- src/routes/design/[id]/edit/index.tsx
+++ src/routes/design/[id]/edit/index.tsx
@@ -12,10 +12,12 @@ export default component$(() => {
           <button class='button bg-purple-bright px-3'>Download</button>
         </div>
       </Navbar>
-      <div class='h-full flex'>
+      <div class='flex h-[calc(100vh-64px)]'>
         <Sidebar />
-        <div class='bg-black-light z-10 w-[350px]'>
-          <button class='bg-black-light flex h-[100px] w-[20px] items-center justify-center rounded-full text-slate-300'>
+        <div
+          class={['bg-black-light relative z-10 w-[350px]', { 'p-0': true }]}
+        >
+          <button class='bg-black-light absolute -right-2 top-[calc(50%-50px)] flex h-[100px] w-[15px] items-center justify-center rounded-full text-slate-300 hover:text-gray-100'>
             <ArrowLeft />
           </button>
         </div>
