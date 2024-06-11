543ef69f3587c6d64eba7ad1063591ce5a8bc292
Francisco <javiermartincuenca@gmail.com>
2024-06-11 20:31:47 +0200

    Add shapes

--- src/presentation/components/sidebar/panels/shapes.tsx
+++ src/presentation/components/sidebar/panels/shapes.tsx
@@ -2,8 +2,19 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <div>
-      <h1 class='text-white'>Shapes</h1>
+    <div class='grid grid-cols-3 gap-2'>
+      <button class='h-[90px] bg-gray-dark'>
+        <span class='sr-only'>Square</span>
+      </button>
+      <button class='h-[90px] rounded-full bg-gray-dark'>
+        <span class='sr-only'>Circle</span>
+      </button>
+      <button
+        style={{ clipPath: 'polygon(50% 0, 100% 100%, 0 100%' }}
+        class='h-[90px] bg-gray-dark'
+      >
+        <span class='sr-only'>Triangle</span>
+      </button>
     </div>
   );
 });
