fa7656165a23d9997bc1410a6e76aae275fe8d34
Francisco <javiermartincuenca@gmail.com>
2024-06-11 22:38:49 +0200

    Text panel

--- src/presentation/components/sidebar/panels/text.tsx
+++ src/presentation/components/sidebar/panels/text.tsx
@@ -2,8 +2,10 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <div>
-      <h1 class='text-white'>Text</h1>
+    <div class='grid grid-cols-1 gap-2'>
+      <div class='cursor-pointer rounded-md bg-gray-dark p-3 text-center text-xl font-bold text-white'>
+        <h2>Add text</h2>
+      </div>
     </div>
   );
 });
