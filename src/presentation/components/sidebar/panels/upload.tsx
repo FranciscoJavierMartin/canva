98ab3ab28fb9559fca5c509b4e6a62f0b9191e3d
Francisco <javiermartincuenca@gmail.com>
2024-06-11 22:10:59 +0200

    Upload input

--- src/presentation/components/sidebar/panels/upload.tsx
+++ src/presentation/components/sidebar/panels/upload.tsx
@@ -1,9 +1,19 @@
 import { component$ } from '@builder.io/qwik';
+import Upload from '@/presentation/icons/upload';
 
 export default component$(() => {
   return (
     <div>
-      <h1 class='text-white'>Upload</h1>
+      <div class='mb-3 flex w-full items-center justify-center rounded-md bg-purple-500 text-white'>
+        <label
+          for='image'
+          class='flex w-full cursor-pointer justify-center gap-2 p-2'
+        >
+          <Upload />
+          Upload image
+        </label>
+        <input type='file' id='image' class='hidden' />
+      </div>
     </div>
   );
 });
