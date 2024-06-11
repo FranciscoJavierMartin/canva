dc4a456787f677c947b1a03e1028db38778dd9ab
Francisco <javiermartincuenca@gmail.com>
2024-06-11 19:59:37 +0200

    Open and close

--- src/presentation/components/sidebar/sidebar-fold.tsx
+++ src/presentation/components/sidebar/sidebar-fold.tsx
@@ -10,16 +10,21 @@ export default component$<SidebarFoldProps>(({ isOpen, closePanel }) => {
   return (
     <div
       class={[
-        'grid-area-sidebar relative z-10 w-[350px] bg-black-light',
+        'grid-area-sidebar relative z-10 w-[350px] bg-black-light transition-transform',
         {
-          'p-0': !isOpen,
-          'px-8 py-5': isOpen,
+          '-translate-x-full p-0': !isOpen,
+          'px-8 py-5 transition-transform': isOpen,
         },
       ]}
     >
       <button
         onClick$={closePanel}
-        class='absolute -right-2 top-[calc(50%-50px)] flex h-[100px] w-[15px] items-center justify-center rounded-full bg-black-light text-slate-300 hover:text-gray-100'
+        class={[
+          'absolute -right-2 top-[calc(50%-50px)] flex h-[100px] w-[15px] items-center justify-center rounded-full bg-black-light text-slate-300 hover:text-gray-100',
+          {
+            hidden: !isOpen,
+          },
+        ]}
       >
         <ArrowLeft />
       </button>
