c00e208b8958b12a97365498335d45e9fc653414
Francisco <javiermartincuenca@gmail.com>
2024-06-12 14:38:42 +0200

    Z index

--- src/presentation/components/sidebars/tools/tools.tsx
+++ src/presentation/components/sidebars/tools/tools.tsx
@@ -9,7 +9,7 @@ export default component$<ToolsProps>(({ component }) => {
   return (
     <aside
       class={[
-        'absolute right-0 h-full w-[250px] bg-black-light text-gray-300 transition-transform duration-300',
+        'absolute right-0 h-full w-[250px] bg-black-light text-gray-300 transition-transform duration-300 z-10',
         {
           'translate-x-full': !component,
           'translate-x-0': !!component,
