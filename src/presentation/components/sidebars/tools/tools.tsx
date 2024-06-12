1f99172c13517f310964c503c59bf1ba600f9328
Francisco <javiermartincuenca@gmail.com>
2024-06-12 18:01:11 +0200

    Update data

--- src/presentation/components/sidebars/tools/tools.tsx
+++ src/presentation/components/sidebars/tools/tools.tsx
@@ -1,22 +1,25 @@
-import { component$ } from '@builder.io/qwik';
-import type { ComponentInfo } from '@/interfaces/types/components';
+import { component$, useComputed$, useContext } from '@builder.io/qwik';
+import { CanvaContext } from '@/presentation/contexts/canva/canva';
 
-type ToolsProps = {
-  component?: ComponentInfo | undefined;
-};
+type ToolsProps = {};
 
-export default component$<ToolsProps>(({ component }) => {
+export default component$<ToolsProps>(() => {
+  const { currentComponent: component, componentData } =
+    useContext(CanvaContext);
+
+  const isOpen = useComputed$(() => !!component);
+
   return (
     <aside
       class={[
         'absolute right-0 z-10 h-full w-[250px] bg-black-light text-gray-300 transition-transform duration-300',
         {
-          'translate-x-full': !component,
-          'translate-x-0': !!component,
+          'translate-x-full': !isOpen.value,
+          'translate-x-0': !!isOpen.value,
         },
       ]}
     >
-      {component && (
+      {component.value && (
         <div class='flex h-full flex-col items-start justify-start gap-6 px-3'>
           <div class='mt-4 flex items-center justify-start gap-4'>
             <span>Color:</span>
@@ -25,12 +28,19 @@ export default component$<ToolsProps>(({ component }) => {
               class='size-[30px] rounded-md'
               style={{
                 backgroundColor:
-                  component.color && component.color !== '#fff'
-                    ? component.color
+                  component.value.color && component.value.color !== '#fff'
+                    ? component.value.color
                     : 'gray',
               }}
             ></label>
-            <input type='color' class='invisible' id='color-input' />
+            <input
+              type='color'
+              class='invisible'
+              id='color-input'
+              onChange$={(event: Event, element: HTMLInputElement) => {
+                componentData.color = element.value;
+              }}
+            />
           </div>
         </div>
       )}
