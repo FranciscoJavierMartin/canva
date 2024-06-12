d78d37ace6360746aaec61d4349caa5b3e65c469
Francisco <javiermartincuenca@gmail.com>
2024-06-12 12:10:05 +0200

    Animate button

--- src/routes/drawer/index.tsx
+++ src/routes/drawer/index.tsx
@@ -1,7 +1,9 @@
 import Navbar from '@/presentation/ui/Navbar';
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 
 export default component$(() => {
+  const openTools = useSignal<boolean>(false);
+
   return (
     <div class='min-w-screen h-screen bg-black'>
       <Navbar>
@@ -20,10 +22,23 @@ export default component$(() => {
         <div class='w-[calc(100vw-85px)] bg-blue-400'>
           <main class='flex h-full flex-col items-center justify-center'>
             <h1 class='text-purple-600'>Hello world</h1>
-            <button class='text-white'>Open tools</button>
+            <button
+              onClick$={() => (openTools.value = !openTools.value)}
+              class='text-white'
+            >
+              Open tools
+            </button>
           </main>
         </div>
-        <aside class='absolute right-0 h-full w-[250px] bg-gray-700'>
+        <aside
+          class={[
+            'absolute h-full w-[250px] bg-gray-700 transition-all duration-1000',
+            {
+              '-right-[250px]': !openTools.value,
+              'right-0': openTools.value,
+            },
+          ]}
+        >
           <h2 class='text-white'>Tools</h2>
         </aside>
       </div>
