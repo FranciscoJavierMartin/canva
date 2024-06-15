442c9c251506b3d9e9bba8a89f54ecf0e44c4c79
Francisco <javiermartincuenca@gmail.com>
2024-06-15 22:36:06 +0200

    Download image

--- src/presentation/components/main-canva/main-canva.tsx
+++ src/presentation/components/main-canva/main-canva.tsx
@@ -7,7 +7,7 @@ export default component$(() => {
 
   return (
     <div class='center-elements h-full w-[calc(100vw-85px)]'>
-      <main class='relative size-auto min-h-[500px] min-w-[650px] overflow-hidden'>
+      <main id='main_design' class='relative size-auto min-h-[500px] min-w-[650px] overflow-hidden'>
         {Object.values(canvaContext.components).map((c) => (
           <CanvaComponent key={c.id} info={c} />
         ))}
--- src/routes/design/[id]/edit/index.tsx
+++ src/routes/design/[id]/edit/index.tsx
@@ -1,17 +1,41 @@
-import { component$ } from '@builder.io/qwik';
+import { $, component$ } from '@builder.io/qwik';
 import Navbar from '@/presentation/ui/Navbar';
 import Sidebar from '@/presentation/components/sidebars/sidebar';
 import Tools from '@/presentation/components/sidebars/tools/tools';
 import MainCanva from '@/presentation/components/main-canva/main-canva';
 import CanvaProvider from '@/presentation/providers/canva-provider';
+import html2canvas from 'html2canvas';
 
 export default component$(() => {
+  const download = $(() => {
+    const mainDesign = document.getElementById('main_design');
+
+    if (mainDesign) {
+      html2canvas(mainDesign, {
+        windowWidth: 650,
+        windowHeight: 500,
+        removeContainer: true,
+      }).then((canvas) => {
+        canvas.toBlob(function (blob) {
+          const link = document.createElement('a');
+          link.download = 'image.png';
+          link.href = URL.createObjectURL(blob!);
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        }, 'image/png');
+      });
+    }
+  });
+
   return (
     <div class='min-w-screen h-screen bg-black'>
       <Navbar>
         <div class='center-elements gap-2 text-gray-200'>
           <button class='button bg-purple-blue px-3'>Save</button>
-          <button class='button bg-purple-bright px-3'>Download</button>
+          <button onClick$={download} class='button bg-purple-bright px-3'>
+            Download
+          </button>
         </div>
       </Navbar>
       <CanvaProvider>
