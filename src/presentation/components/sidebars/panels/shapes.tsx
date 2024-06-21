4d906711cdbb636e633a121b39bdf686a45563bf
Francisco <javiermartincuenca@gmail.com>
2024-06-21 20:20:22 +0200

    style: shapes dark mode

--- src/presentation/components/sidebars/panels/shapes.tsx
+++ src/presentation/components/sidebars/panels/shapes.tsx
@@ -7,20 +7,20 @@ export default component$(() => {
   return (
     <div class='grid grid-cols-3 gap-2'>
       <button
-        class='size-[90px] bg-gray-dark transition-colors hover:bg-gray-mid'
+        class='size-[90px] bg-gray-light transition-colors hover:bg-gray-mid dark:bg-gray-dark'
         onClick$={() => canvaContext.createShape('rect')}
       >
         <span class='sr-only'>Square</span>
       </button>
       <button
-        class='size-[90px] rounded-full bg-gray-dark transition-colors hover:bg-gray-mid'
+        class='size-[90px] rounded-full bg-gray-light transition-colors hover:bg-gray-mid dark:bg-gray-dark'
         onClick$={() => canvaContext.createShape('circle')}
       >
         <span class='sr-only'>Circle</span>
       </button>
       <button
         style={{ clipPath: 'polygon(50% 0, 100% 100%, 0 100%' }}
-        class='size-[90px] bg-gray-dark transition-colors hover:bg-gray-mid'
+        class='size-[90px] bg-gray-light transition-colors hover:bg-gray-mid dark:bg-gray-dark'
         onClick$={() => canvaContext.createShape('triangle')}
       >
         <span class='sr-only'>Triangle</span>
