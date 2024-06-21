265cb2251de0fa7e022ea1d9a554992943997e0b
Francisco <javiermartincuenca@gmail.com>
2024-06-21 20:13:32 +0200

    style: background panel dark mode

--- src/presentation/ui/Navbar.tsx
+++ src/presentation/ui/Navbar.tsx
@@ -4,7 +4,7 @@ import Logo from '@/presentation/icons/logo';
 
 export default component$(() => {
   return (
-    <header class='bg-white z-30 w-full shadow-md dark:bg-black-mid'>
+    <header class='z-30 w-full bg-white shadow-md dark:bg-black-mid'>
       <div class='m-auto p-3'>
         <nav class='flex items-center justify-between'>
           <Link href='/home' class='h-[30px] w-[80px]'>
